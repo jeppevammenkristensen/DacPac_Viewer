42dd16ab3a52bc23d3e79a238f00ef2f64c6f41b
Jeppe Roi Kristensen <jeppe@jeppek.dk>
2026-07-21 21:48:47 +0200

    Fixed issue with syntax highlighting. Prefering default when theme is light

--- source/DacPac.UI/Views/Displays/TableDisplay.axaml.cs
+++ source/DacPac.UI/Views/Displays/TableDisplay.axaml.cs
@@ -1,7 +1,6 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
-using AvaloniaEdit.Highlighting;
 using DacPac.UI.Infrastructure;
 using DacPac.UI.ViewModels.Displays;
 
@@ -14,6 +13,7 @@ public partial class TableDisplay : UserControl
         InitializeComponent();
 
         DataContextChanged += OnDataContextChanged;
+        ActualThemeVariantChanged += OnActualThemeVariantChanged;
     }
 
     private void OnDataContextChanged(object? sender, System.EventArgs e)
@@ -21,7 +21,10 @@ public partial class TableDisplay : UserControl
         if (DataContext is TableDisplayViewModel viewModel)
         {
             ScriptEditor.Text = viewModel.Script;
-            ScriptEditor.SyntaxHighlighting = CodeSyntaxHighlighting.For(viewModel.Script);
+            ScriptEditor.SyntaxHighlighting = CodeSyntaxHighlighting.Sql;
         }
     }
+
+    private void OnActualThemeVariantChanged(object? sender, System.EventArgs e)
+        => ScriptEditor.SyntaxHighlighting = CodeSyntaxHighlighting.Sql;
 }
