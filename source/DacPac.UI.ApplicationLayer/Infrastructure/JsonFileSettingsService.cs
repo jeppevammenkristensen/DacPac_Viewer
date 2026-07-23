1baa21ab5727a2a934956cc9b76c65041cc8c7d3
Jeppe Roi Kristensen <jeppe@jeppek.dk>
2026-07-24 01:25:52 +0200

    Updated update manager to output downloading

--- source/DacPac.UI.ApplicationLayer/Infrastructure/JsonFileSettingsService.cs
+++ source/DacPac.UI.ApplicationLayer/Infrastructure/JsonFileSettingsService.cs
@@ -2,65 +2,12 @@ using System.Collections.Immutable;
 using System.IO.Abstractions;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Text.Json.Serialization.Metadata;
 using DacPac.Core;
 using CommunityToolkit.Mvvm.Messaging;
 using Microsoft.Extensions.Logging;
 using TruePath;
 
 namespace DacPac.UI.ApplicationLayer.Infrastructure;
-
-public static class JsonSettingsExtensions
-{
-    public static JsonSettingsWrapper<TData> WrapperFromTypeInfo<TData>(this JsonTypeInfo<TData> typeInfo,
-        AbsolutePath filePath, IFileSystem fileSystem, Func<TData> createEmpty)
-    {
-        return new JsonSettingsWrapper<TData>(filePath, typeInfo, fileSystem, createEmpty);
-    }
-}
-
-public class JsonSettingsWrapper<TData>
-{
-    private readonly AbsolutePath _filePath;
-    private readonly JsonTypeInfo<TData> _typeInfo;
-    private readonly IFileSystem _fileSystem;
-    private readonly Func<TData> _createEmpty;
-
-    public JsonSettingsWrapper(AbsolutePath filePath, JsonTypeInfo<TData> typeInfo, IFileSystem fileSystem, Func<TData> createEmpty)
-    {
-        _filePath = filePath;
-        _typeInfo = typeInfo;
-        _fileSystem = fileSystem;
-        _createEmpty = createEmpty;
-    }
-    
-    public TData Load()
-    {
-        try
-        {
-            if (!_filePath.FileExists(_fileSystem)) return _createEmpty();
-            var json = _filePath.ReadAllText(_fileSystem);
-            return JsonSerializer.Deserialize(json, _typeInfo) ?? _createEmpty();
-        }
-        catch (Exception ex)
-        {
-            return _createEmpty();
-        }
-    }
-    public void Save(TData data)
-    {
-        try
-        {
-            var directory = _filePath / "..";
-            directory.CreateDirectory(_fileSystem);
-            _fileSystem.File.WriteAllText(_filePath, JsonSerializer.Serialize(data, _typeInfo));
-        }
-        catch (Exception ex)
-        {
-        }
-    }
-}
-
 
 public partial class JsonFileSettingsService : ISettingsService
 {
---
+++ source/DacPac.UI.ApplicationLayer/Infrastructure/JsonSettingsWrapper.cs
@@ -0,0 +1,48 @@
+﻿using System.IO.Abstractions;
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+using TruePath;
+
+namespace DacPac.UI.ApplicationLayer.Infrastructure;
+
+public class JsonSettingsWrapper<TData>
+{
+    private readonly AbsolutePath _filePath;
+    private readonly JsonTypeInfo<TData> _typeInfo;
+    private readonly IFileSystem _fileSystem;
+    private readonly Func<TData> _createEmpty;
+
+    public JsonSettingsWrapper(AbsolutePath filePath, JsonTypeInfo<TData> typeInfo, IFileSystem fileSystem, Func<TData> createEmpty)
+    {
+        _filePath = filePath;
+        _typeInfo = typeInfo;
+        _fileSystem = fileSystem;
+        _createEmpty = createEmpty;
+    }
+    
+    public TData Load()
+    {
+        try
+        {
+            if (!_filePath.FileExists(_fileSystem)) return _createEmpty();
+            var json = _filePath.ReadAllText(_fileSystem);
+            return JsonSerializer.Deserialize(json, _typeInfo) ?? _createEmpty();
+        }
+        catch (Exception ex)
+        {
+            return _createEmpty();
+        }
+    }
+    public void Save(TData data)
+    {
+        try
+        {
+            var directory = _filePath / "..";
+            directory.CreateDirectory(_fileSystem);
+            _fileSystem.File.WriteAllText(_filePath, JsonSerializer.Serialize(data, _typeInfo));
+        }
+        catch (Exception ex)
+        {
+        }
+    }
+}
