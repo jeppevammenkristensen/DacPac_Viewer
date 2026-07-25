d71ee6619811e981dd9aca9f7156917e21d2f00b
Jeppe Roi Kristensen <jeppe@jeppek.dk>
2026-07-26 00:29:13 +0200

    Add View display support and refactor column wrapper logic

---
+++ source/DacPac.UI/Views/Displays/ViewDisplay.axaml.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using DacPac.UI.Infrastructure;
+using DacPac.UI.ViewModels.Displays;
+
+namespace DacPac.UI.Views.Displays;
+
+public partial class ViewDisplay : UserControl
+{
+    public ViewDisplay()
+    {
+        InitializeComponent();
+        DataContextChanged += OnDataContextChanged;
+        ActualThemeVariantChanged += OnActualThemeVariantChanged;
+    }
+
+    private void OnDataContextChanged(object? sender, System.EventArgs e)
+    {
+        if (DataContext is ViewDisplayViewModel viewModel)
+        {
+            ScriptEditor.Text = viewModel.Script;
+            ScriptEditor.SyntaxHighlighting = CodeSyntaxHighlighting.Sql;
+        }
+    }
+
+    private void OnActualThemeVariantChanged(object? sender, System.EventArgs e)
+        => ScriptEditor.SyntaxHighlighting = CodeSyntaxHighlighting.Sql;
+}
