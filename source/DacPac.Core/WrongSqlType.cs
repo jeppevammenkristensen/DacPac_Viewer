7215700d9b16f2eee70280c71e94bc46167d65e5
Jeppe Roi Kristensen <jeppe@jeppek.dk>
2026-07-26 00:29:13 +0200

    Add View display support and refactor column wrapper logic

---
+++ source/DacPac.Core/WrongSqlType.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.SqlServer.Dac.Model;
+
+namespace DacPac.Core;
+
+public static class WrongSqlType
+{
+    /// <summary>
+    /// Throws an exception if the <param name="model"></param> is not of the correct type <param name="typeClass"></param>.
+    /// </summary>
+    /// <param name="model"></param>    
+    /// <param name="typeClass"></param>
+    /// <param name="paramName"></param>
+    public static void ThrowIfIncorrectType(this TSqlObject model, ModelTypeClass typeClass, [CallerArgumentExpression(nameof(model))] string? paramName = null)
+    {
+        if (model.ObjectType != typeClass)
+        {
+            throw new ArgumentException($"The provided TSqlObject is not of the expected type. Expected type: {typeClass.Name}, but got: {model.ObjectType.Name}", paramName);
+            throw new InvalidOperationException($"The provided TSqlObject is not of the expected type. Expected type: {typeClass.Name}, but got: {model.ObjectType.Name}");
+        }
+    }
+}
