59a414015c7b01490a175a827aafe946231de0a2
Jeppe Roi Kristensen <jeppe@jeppek.dk>
2026-07-28 23:13:02 +0200

    Minor adjustments

--- source/Tests/DacPac.Core.Tests/TableToCsharpClassGeneratorTest.cs
+++ source/Tests/DacPac.Core.Tests/TableToCsharpClassGeneratorTest.cs
@@ -8,22 +8,33 @@ namespace DacPac.Core.Tests;
 public class TableToCsharpClassGeneratorTest
 {
     [Fact]
-    public void Build_GeneratesPropertiesWithMappedTypes()
+    public void Build_GeneratesPropertiesAndForeignKeyRemarks()
     {
         using var model = new TSqlModel(SqlServerVersion.Sql160, new TSqlModelOptions());
         model.AddObjects("""
+                         CREATE TABLE [dbo].[Customer]
+                         (
+                             [CustomerId] int NOT NULL
+                         );
+                         GO
                          CREATE TABLE [dbo].[Orders]
                          (
                              [OrderId] bigint NOT NULL,
-                             [CustomerId] int NULL
+                             [CustomerId] int NULL,
+                             CONSTRAINT [FK_Orders_Customer] FOREIGN KEY ([CustomerId]) REFERENCES [dbo].[Customer] ([CustomerId])
                          );
                          """);
 
-        var orders = model.GetObjects(DacQueryScopes.UserDefined, Table.TypeClass).Single();
+        var tables = model.GetObjects(DacQueryScopes.UserDefined, Table.TypeClass).ToList();
+        var orders = tables.Single(x => x.Name.Parts.Last() == "Orders");
+        var customer = tables.Single(x => x.Name.Parts.Last() == "Customer");
         var output = new TableToCsharpClassGenerator().Build(orders).ToString();
+        var customerOutput = new TableToCsharpClassGenerator().Build(customer).ToString();
 
         Assert.Contains("public class Orders", output);
         Assert.Contains("public long OrderId { get; set; }", output);
         Assert.Contains("public int? CustomerId { get; set; }", output);
+        Assert.Contains("Foreign key pointing to [dbo].[Customer].[CustomerId] in [dbo].[Customer]", output);
+        Assert.Contains("Key referenced by [dbo].[Orders].[CustomerId] in [dbo].[Orders]", customerOutput);
     }
 }
