5c7eb57abd74ba7b4d5ba03277a2358a6bcd20e2
Jeppe Roi Kristensen <jeppe@jeppek.dk>
2026-08-05 01:32:19 +0200

    Enhance combobox logic

--- source/DacPac.UI/Views/LandingPage/LandingPageControl.axaml.cs
+++ source/DacPac.UI/Views/LandingPage/LandingPageControl.axaml.cs
@@ -11,9 +11,19 @@ public partial class LandingPageControl : UserControl
         ResultsGrid.KeyDown += ResultsGridOnKeyDown;
     }
 
+    private void ClearComboBoxSelection(object? sender, SelectionChangedEventArgs e)
+    {
+        // This is done to enforce that the Placeholder text (for instance 1 selected) is "always" displayed
+        
+        if (sender is ComboBox {SelectedIndex: not -1} comboBox)
+        {
+            comboBox.Clear();
+        }
+    }
+
     private void ResultsGridOnKeyDown(object? sender, KeyEventArgs e)
     {
-        if (e.Key == Key.A && e.KeyModifiers == KeyModifiers.Control)
+        if (e is {Key: Key.A, KeyModifiers: KeyModifiers.Control})
         {
             ResultsGrid.SelectAll();
             e.Handled = true;
